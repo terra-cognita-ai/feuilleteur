285bb2c055a93c3178ca0dc8e5f26784d682bb2c
leokeba <leokeba@gmail.com>
2024-09-04 19:41:02 +0200

    ollama config

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,3 +1,3 @@
 MODEL="gemma2" # gpt-4o or gemma2 for example
 PROVIDER="ollama" # use openai or ollama
-HOST="http://192.168.1.60:1143" # API URL for ollama if not localhost
+HOST="http://192.168.1.29:11434" # API URL for ollama if not localhost
